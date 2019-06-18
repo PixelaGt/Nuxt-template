1de2278a43d22180d51f2e04320932854ee3f99b
SixPack <EstuardoA25@gmail.com>
2019-06-18 12:32:34 -0600

    First commit

---
+++ app/components/prismic/PrismicImage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'PrismicImage',
+  props: {
+    field: {
+      default: null,
+      required: true,
+      validator: prop => typeof prop === 'object' || prop === null
+    }
+  },
+  render(h) {
+    if (this.field === null || this.field === undefined) return null
+    return <img src={this.field.url} alt={this.field.alt || ''} copyright={this.field.copyright || ''} />
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,87 @@
+const i18n = require('./i18n')
+
+module.exports = {
+  srcDir: 'app/',
+  mode: 'universal',
+  env: {
+    PRISMIC_ENDPOINT: 'https://def-nuxt-template.cdn.prismic.io/api/v2'
+  },
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    meta: [
+      { charset: 'utf-8' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    title: process.env.npm_package_name || ''
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~/assets/styles/index.scss'
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '@/plugins/prismic', ssr: true },
+    { src: '@/plugins/prismic-components', ssr: true }
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/style-resources',
+    '@nuxtjs/eslint-module',
+    ['nuxt-i18n', i18n]
+  ],
+  /*
+
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+
+  /*
+  ** Style resources module configuration
+  */
+  styleResources: {
+    scss: [
+      '@/assets/styles/index.scss'
+    ]
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      /*
+      * This alias is necesary to work with Prismic-vue
+      * because prismic-vue requires template compiler
+      */
+      config.resolve.alias.vue = 'vue/dist/vue.common'
+    }
+  }
+}
