538b17bb4f08ffe9d92169dd6dda37e2818d034f
SixPack <EstuardoA25@gmail.com>
2019-07-01 15:45:36 -0600

    Added class to nuxt-link component

--- app/plugins/prismic.js
+++ app/plugins/prismic.js
@@ -82,20 +82,20 @@ export default ({ app }, inject) => {
   }
 
   function htmlSerializer(type, element, content) {
-    // Generate links to Prismic Documents as <router-link> components
+    // Generate links to Prismic Documents as <nuxt-link> components
     // Present by default, it is recommended to keep this
     if (type === Elements.hyperlink) {
       const url = PrismicDOM.Link.url(element.data, linkResolver)
 
       if (element.data.link_type === 'Document') {
-        return `<nuxt-link to="${url}">${content}</nuxt-link>`
+        return `<nuxt-link class="Nuxt" to="${url}">${content}</nuxt-link>`
       } else {
         const target = element.data.target ? `target="'${element.data.target}'" rel="noopener"` : ''
         return `<a href="${url}" ${target}>${content}</a>`
       }
     }
 
-    // If the image is also a link to a Prismic Document, it will return a <router-link> component
+    // If the image is also a link to a Prismic Document, it will return a <nuxt-link> component
     // Present by default, it is recommended to keep this
     if (type === Elements.image) {
       let result = `<img src="${element.url}" alt="${element.alt || ''}" copyright="${element.copyright || ''}">`
@@ -104,7 +104,7 @@ export default ({ app }, inject) => {
         const url = PrismicDOM.Link.url(element.linkTo, linkResolver)
 
         if (element.linkTo.link_type === 'Document') {
-          result = `<nuxt-link to="${url}">${result}</nuxt-link>`
+          result = `<nuxt-link class="Nuxt" to="${url}">${result}</nuxt-link>`
         } else {
           const target = element.linkTo.target ? `target="${element.linkTo.target}" rel="noopener"` : ''
           result = `<a href="${url}" ${target}>${result}</a>`
